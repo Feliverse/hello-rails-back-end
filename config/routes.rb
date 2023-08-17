f3c331b312e6b22bb9f26451a09882c79f027eb8
Feliverse <114632980+Feliverse@users.noreply.github.com>
2023-08-17 11:13:36 -0400

    Debug name of the table greeting

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   
-  # /api/v1/grettings
+  # /api/v1/greetings
   namespace :api do
     namespace :v1 do
-      resources :grettings
+      resources :greetings
     end
   end
   
--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_17_132605) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "grettings", force: :cascade do |t|
+  create_table "greetings", force: :cascade do |t|
     t.string "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
